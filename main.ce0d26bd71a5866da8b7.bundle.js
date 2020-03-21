1d96114d018da753f8ccfc641af8c0690a915231
Muli Kaminker <kaminker10@gmail.com>
2020-03-21 14:53:36 +0200

    added storybook

---
+++ main.ce0d26bd71a5866da8b7.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,o,p){p(418),p(560),p(1232),n.exports=p(1267)},function(n,o){}],[[0,1,2]]]);
---
+++ precache-manifest.0d18e253174dd98bbfc73849921dbd36.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c353c01aee6e966c14ac32e758fb4f3",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3ced690d93a416b78912.bundle.js"
+  },
+  {
+    "url": "runtime~main.3ced690d93a416b78912.bundle.js"
+  },
+  {
+    "url": "vendors~main.3ced690d93a416b78912.bundle.js"
+  },
+  {
+    "url": "vendors~main.3ced690d93a416b78912.bundle.js.LICENSE.txt"
+  }
+]);
