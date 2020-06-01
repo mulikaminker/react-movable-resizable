5ec664297f749875bc8393024058e0f467430ad3
Shmuel Kaminker <shmuel.kaminker@MacBook-Pro.local>
2020-06-01 12:41:03 +0300

    fix initialX and initialY bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,26 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { ContextProvider} from './context/movable-resizble.context';
-import Movable from './Movable';
-import Resizble from './Resizable';
-import { ResizbleProps } from './types';
+import { ContextProvider } from "./context/movable-resizble.context";
+import Movable from "./Movable";
+import Resizble from "./Resizable";
+import { ResizbleProps } from "./types";
 
 export default React.forwardRef((props: ResizbleProps, ref) => {
-
-	console.log(ref)
-
-	return (
-		<ContextProvider ref={ref}>
-			<Movable {...props}>
-				<Resizble {...props} />
-			</Movable>
-		</ContextProvider>
-	);
+    return (
+        <ContextProvider ref={ref}>
+            <Movable {...props}>
+                <Resizble {...props} />
+            </Movable>
+        </ContextProvider>
+    );
 });
 
-export const MovableComponent = React.forwardRef((props: ResizbleProps, ref) =>  {
-	return (
-		<ContextProvider ref={ref}>
-			<Movable {...props}/>
-		</ContextProvider>
-	);
-})
+export const MovableComponent = React.forwardRef(
+    (props: ResizbleProps, ref) => {
+        return (
+            <ContextProvider ref={ref}>
+                <Movable {...props} />
+            </ContextProvider>
+        );
+    }
+);
