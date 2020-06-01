e4bc28f2c02109ff3a1dfc6e9319318bfa68f9b5
Shmuel Kaminker <shmuel.kaminker@MacBook-Pro.local>
2020-06-01 12:39:29 +0300

    fix initialX and initialY bug

--- example/src/App.js
+++ example/src/App.js
@@ -1,25 +1,31 @@
-import React from 'react'
+import React from "react";
 
-
-import ReactMovableResizable, {Movable} from 'react-movable-resizable';
-
+import ReactMovableResizable, { Movable } from "react-movable-resizable";
 
 export default () => {
     return (
-      <div>
-
-      <div style={{ width: '500px', height: '500px', border: '3px #ccc solid', position: 'relative'}}>
-        <ReactMovableResizable useParentBounds={true}
-        gridBackground
-        initialWidth={200}
-        initialHeight={200}
-        initialX={500}
-        initialY={10}
-         borderColor="red"
-          handlersColor="red" width={200} height={300}>
-        </ReactMovableResizable>
-      </div >
-      </div>
-
+        <div>
+            <div
+                style={{
+                    width: "500px",
+                    height: "500px",
+                    border: "3px #ccc solid",
+                    position: "relative"
+                }}
+            >
+                <ReactMovableResizable
+                    useParentBounds={false}
+                    gridBackground
+                    initialWidth={200}
+                    initialHeight={200}
+                    initialX={500}
+                    initialY={10}
+                    borderColor="red"
+                    handlersColor="red"
+                    width={200}
+                    height={300}
+                ></ReactMovableResizable>
+            </div>
+        </div>
     );
-  }
+};
--- src/Movable.tsx
+++ src/Movable.tsx
@@ -1,230 +1,269 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { Context } from './context/movable-resizble.context';
-import { calcElemInnerBox, getResizableOffsets, getPropertyStyleValueByProp} from './utils/general';
+import { Context } from "./context/movable-resizble.context";
+import {
+    calcElemInnerBox,
+    getResizableOffsets,
+    getPropertyStyleValueByProp
+} from "./utils/general";
 
-import { MovableStyled } from './ReactMovableResizble.styles';
+import { MovableStyled } from "./ReactMovableResizble.styles";
 
 export type MovableProps = {
-	useParentBounds?: boolean;
-	hideBorder?: boolean;
-	children?: React.ReactNode;
-	hideHandlers?: boolean;
-	borderColor?: string;
-	gridBackground?: boolean;
-	onDrag?: Function;
-	onMouseUp?: Function;
-	initialWidth?: number,
-	initialHeight?: number,
-	initialX?: number,
-	initialY?: number,
-	width?: number,
-
+    useParentBounds?: boolean;
+    hideBorder?: boolean;
+    children?: React.ReactNode;
+    hideHandlers?: boolean;
+    borderColor?: string;
+    gridBackground?: boolean;
+    onDrag?: Function;
+    onMouseUp?: Function;
+    initialWidth?: number;
+    initialHeight?: number;
+    initialX?: number;
+    initialY?: number;
+    width?: number;
 };
 
-const Movable = ({ useParentBounds,
-	 children,
-	 hideBorder,
-	 borderColor = '#000',
-	 onDrag = () => {},
-	 initialWidth = 100,
-	 initialHeight = 100,
-	 initialX,
-	 initialY,
-	 onMouseUp = () => {},
-	 gridBackground = false}: MovableProps) => {
-	const {
-		positions,
-		setPositions,
-		offsets,
-		setOffsets,
-		setMovableActive,
-		resizbleActive,
-		movableRef
-	} = React.useContext(Context);
+const Movable = ({
+    useParentBounds,
+    children,
+    hideBorder,
+    borderColor = "#000",
+    onDrag = () => {},
+    initialWidth = 100,
+    initialHeight = 100,
+    initialX,
+    initialY,
+    onMouseUp = () => {},
+    gridBackground = false
+}: MovableProps) => {
+    const {
+        positions,
+        setPositions,
+        offsets,
+        setOffsets,
+        setMovableActive,
+        resizbleActive,
+        movableRef
+    } = React.useContext(Context);
 
-	React.useEffect(() => {
-		const movableEl = movableRef.current;
-		const {offsetTop, offsetBottom, offsetLeft, offsetRight} = getResizableOffsets(movableEl, movableEl.parentNode)
-		onDrag(null, {...positions, offsetTop, offsetBottom, offsetLeft, offsetRight})
-	}, [positions])
+    React.useEffect(() => {
+        const movableEl = movableRef.current;
+        const {
+            offsetTop,
+            offsetBottom,
+            offsetLeft,
+            offsetRight
+        } = getResizableOffsets(movableEl, movableEl.parentNode);
+        onDrag(null, {
+            ...positions,
+            offsetTop,
+            offsetBottom,
+            offsetLeft,
+            offsetRight
+        });
+    }, [positions]);
 
-	React.useEffect(() => {
-		setPositions({...positions, width: initialWidth, height: initialHeight, x: initialX, y: initialY})
-	}, [initialY, initialWidth, initialX, initialHeight])
+    React.useEffect(() => {
+        setPositions({
+            ...positions,
+            width: initialWidth,
+            height: initialHeight,
+            x: initialX,
+            y: initialY
+        });
+        setOffsets({
+            x: initialX,
+            y: initialY
+        });
+    }, [initialY, initialWidth, initialX, initialHeight]);
 
-	const getMovableParentBounds = ({
-		newX,
-		newY
-	}: {
-		newX: number;
-		newY: number;
-	}): { xPosition: number; yPosition: number } => {
-		const rect = movableRef.current.getBoundingClientRect();
-		const movableEl = movableRef.current;
-		const movableParent = movableEl.parentNode.getBoundingClientRect();
-		let xPosition = newX,
-			yPosition = newY;
+    const getMovableParentBounds = ({
+        newX,
+        newY
+    }: {
+        newX: number;
+        newY: number;
+    }): { xPosition: number; yPosition: number } => {
+        const rect = movableRef.current.getBoundingClientRect();
+        const movableEl = movableRef.current;
+        const movableParent = movableEl.parentNode.getBoundingClientRect();
+        let xPosition = newX,
+            yPosition = newY;
 
-		const parentInnerBoxSpacing = calcElemInnerBox(movableEl.parentNode)
+        const parentInnerBoxSpacing = calcElemInnerBox(movableEl.parentNode);
 
-		const maxParentleft = movableParent.left - rect.left;
-		const maxParentRight = (movableParent.right - movableParent.left) - rect.width - parentInnerBoxSpacing;
-		const maxParentTop = movableParent.top - rect.top - parentInnerBoxSpacing;
-		const maxParentBottom = movableParent.bottom - movableParent.top - rect.height - parentInnerBoxSpacing;
+        const maxParentleft = movableParent.left - rect.left;
+        const maxParentRight =
+            movableParent.right -
+            movableParent.left -
+            rect.width -
+            parentInnerBoxSpacing;
+        const maxParentTop =
+            movableParent.top - rect.top - parentInnerBoxSpacing;
+        const maxParentBottom =
+            movableParent.bottom -
+            movableParent.top -
+            rect.height -
+            parentInnerBoxSpacing;
 
+        if (newX < maxParentleft) {
+            xPosition = 0;
+        }
 
-		if (newX < maxParentleft) {
-			xPosition = 0;
-		}
+        if (newX > maxParentRight) {
+            xPosition = maxParentRight;
+        }
 
-		if (newX > maxParentRight) {
-			xPosition = maxParentRight;
-		}
+        if (newY < maxParentTop) {
+            yPosition = 0;
+        }
 
-		if (newY < maxParentTop) {
-			yPosition = 0;
-		}
+        if (newY > maxParentBottom) {
+            yPosition = maxParentBottom;
+        }
 
-		if (newY > maxParentBottom) {
-			yPosition = maxParentBottom;
-		}
+        return { xPosition, yPosition };
+    };
 
-		return { xPosition, yPosition };
-	};
+    const onMovableTouchStart = (e: TouchEvent) => {
+        const event = e.touches[0];
 
-	const onMovableTouchStart = (e: TouchEvent) => {
+        let newX: number, newY: number, prevX: number, prevY: number;
+        prevX = event.clientX - offsets.x;
+        prevY = event.clientY - offsets.y;
 
-		const event = e.touches[0]
+        const onMovableTouchMove = (e: TouchEvent): void => {
+            const event = e.touches[0];
 
-		let newX: number,
-			newY: number,
-			prevX = 0,
-			prevY = 0;
-			prevX = event.clientX - offsets.x;
-			prevY = event.clientY - offsets.y;
+            newX = event.clientX - prevX;
+            newY = event.clientY - prevY;
 
-				const onMovableTouchMove = (e: TouchEvent): void => {
+            const movableEl = movableRef.current;
 
-					const event = e.touches[0]
+            const {
+                offsetTop,
+                offsetBottom,
+                offsetLeft,
+                offsetRight
+            } = getResizableOffsets(movableEl, movableEl.parentNode);
 
-					newX = event.clientX - prevX;
-					newY = event.clientY - prevY;
+            if (useParentBounds) {
+                const { xPosition, yPosition } = getMovableParentBounds({
+                    newX,
+                    newY
+                });
+                (newX = xPosition), (newY = yPosition);
+            }
 
-					const movableEl = movableRef.current;
+            setPositions({
+                ...positions,
+                x: newX,
+                y: newY,
+                offsetTop,
+                offsetBottom,
+                offsetLeft,
+                offsetRight,
+                right: getPropertyStyleValueByProp(movableEl, "right"),
+                left: getPropertyStyleValueByProp(movableEl, "left")
+            });
 
-					const {offsetTop, offsetBottom, offsetLeft, offsetRight} = getResizableOffsets(movableEl, movableEl.parentNode)
+            setOffsets({
+                x: newX,
+                y: newY
+            });
+        };
 
+        const onMovableTouchEnd = () => {
+            document.removeEventListener("touchmove", onMovableTouchMove);
+            document.removeEventListener("touchend", onMovableTouchEnd);
+        };
 
-					if (useParentBounds) {
-						const { xPosition, yPosition } = getMovableParentBounds({ newX, newY });
-						(newX = xPosition), (newY = yPosition);
-					}
+        document.addEventListener("touchmove", onMovableTouchMove);
+        document.addEventListener("touchend", onMovableTouchEnd);
+    };
 
-					setPositions({
-						...positions,
-						x: newX,
-						y: newY,
-						offsetTop, offsetBottom, offsetLeft, offsetRight,
-						right: getPropertyStyleValueByProp(movableEl, 'right'),
-						left: getPropertyStyleValueByProp(movableEl, 'left')
-					});
+    const onMovableMouseDown = (e: MouseEvent) => {
+        if (resizbleActive) return;
+        e.preventDefault();
 
-					setOffsets({
-									x: newX,
-									y: newY
-					});
+        let newX: number, newY: number, prevX: number, prevY: number;
+        prevX = e.clientX - offsets.x;
+        prevY = e.clientY - offsets.y;
 
-				}
+        const onMovableMouseMove = (e: MouseEvent): void => {
+            newX = e.clientX - prevX;
+            newY = e.clientY - prevY;
 
-				const onMovableTouchEnd = () => {
+            const movableEl = movableRef.current;
 
-					document.removeEventListener('touchmove', onMovableTouchMove);
-					document.removeEventListener('touchend', onMovableTouchEnd);
+            const {
+                offsetTop,
+                offsetBottom,
+                offsetLeft,
+                offsetRight
+            } = getResizableOffsets(movableEl, movableEl.parentNode);
 
-				}
+            if (useParentBounds) {
+                const { xPosition, yPosition } = getMovableParentBounds({
+                    newX,
+                    newY
+                });
+                (newX = xPosition), (newY = yPosition);
+            }
 
+            setPositions({
+                ...positions,
+                x: newX,
+                y: newY,
+                offsetTop,
+                offsetBottom,
+                offsetLeft,
+                offsetRight,
+                right: getPropertyStyleValueByProp(movableEl, "right"),
+                left: getPropertyStyleValueByProp(movableEl, "left")
+            });
 
-		document.addEventListener('touchmove', onMovableTouchMove);
-		document.addEventListener('touchend', onMovableTouchEnd);
+            setOffsets({
+                x: newX,
+                y: newY
+            });
+        };
 
-	}
+        const onMovableMouseUp = (e: MouseEvent) => {
+            onMouseUp(e);
+            document.removeEventListener("mousemove", onMovableMouseMove);
+            document.removeEventListener("mouseup", onMovableMouseUp);
 
-	const onMovableMouseDown = (e: MouseEvent) => {
-		if (resizbleActive) return;
-		e.preventDefault();
+            prevX = newX;
+            prevY = newY;
+            setMovableActive(false);
+        };
 
-		let newX: number,
-			newY: number,
-			prevX = 0,
-			prevY = 0;
-			prevX = e.clientX - offsets.x;
-			prevY = e.clientY - offsets.y;
+        document.addEventListener("mousemove", onMovableMouseMove);
+        document.addEventListener("mouseup", onMovableMouseUp);
 
-		const onMovableMouseMove = (e: MouseEvent): void => {
-			newX = e.clientX - prevX;
-			newY = e.clientY - prevY;
+        setMovableActive(true);
+    };
 
-			const movableEl = movableRef.current;
-
-			const {offsetTop, offsetBottom, offsetLeft, offsetRight} = getResizableOffsets(movableEl, movableEl.parentNode)
-
-
-			if (useParentBounds) {
-				const { xPosition, yPosition } = getMovableParentBounds({ newX, newY });
-				(newX = xPosition), (newY = yPosition);
-			}
-
-			setPositions({
-				...positions,
-				x: newX,
-				y: newY,
-				offsetTop, offsetBottom, offsetLeft, offsetRight,
-				right: getPropertyStyleValueByProp(movableEl, 'right'),
-				left: getPropertyStyleValueByProp(movableEl, 'left')
-			});
-
-			setOffsets({
-							x: newX,
-							y: newY
-			});
-		};
-
-		const onMovableMouseUp = (e: MouseEvent) => {
-			onMouseUp(e);
-			document.removeEventListener('mousemove', onMovableMouseMove);
-			document.removeEventListener('mouseup', onMovableMouseUp);
-
-
-			prevX = newX;
-			prevY = newY;
-			setMovableActive(false);
-		};
-
-		  document.addEventListener('mousemove', onMovableMouseMove);
-			document.addEventListener('mouseup', onMovableMouseUp);
-
-		setMovableActive(true);
-	};
-
-	return (
-		<MovableStyled
-			ref={movableRef}
-			onMouseDown={onMovableMouseDown}
-			onTouchStart={onMovableTouchStart}
-			width={positions.width}
-			height={positions.height}
-			x={positions.x}
-			y={positions.y}
-			maxHeight={positions.maxHeight}
-			hideBorder={hideBorder}
-			borderColor={borderColor}
-			gridBackground={gridBackground}
-
-		>
-			{children}
-		</MovableStyled>
-	);
+    return (
+        <MovableStyled
+            ref={movableRef}
+            onMouseDown={onMovableMouseDown}
+            onTouchStart={onMovableTouchStart}
+            width={positions.width}
+            height={positions.height}
+            x={positions.x}
+            y={positions.y}
+            maxHeight={positions.maxHeight}
+            hideBorder={hideBorder}
+            borderColor={borderColor}
+            gridBackground={gridBackground}
+        >
+            {children}
+        </MovableStyled>
+    );
 };
 
 export default Movable;
