ac949ca68191baab740fba6bbfb160e396161bad
shmuel kaminker <shmuel.kaminker@brightmachines.com>
2020-10-30 16:44:07 +0200

    version 1.0.37 - added classNamne and style props

--- src/types.tsx
+++ src/types.tsx
@@ -1,50 +1,49 @@
 export type ResizbleProps = {
-	useParentBounds?: boolean;
-	maxWidth?: number;
-	maxHeight?: number;
-	width?: number;
-	height?: number;
-	className?: string;
-	children?: React.ReactNode;
-	 hideHandlers?: boolean;
-	 hideBorder?: boolean;
-	 handlersColor?: string;
-	 borderColor?: string;
-	 gridBackground?: boolean;
-	 onDrag?: Function;
-	 initialWidth?: number,
-	 initialHeight?: number,
-	 initialX?: number,
-	initialY?: number,
+    useParentBounds?: boolean;
+    maxWidth?: number;
+    maxHeight?: number;
+    width?: number;
+    height?: number;
+    className?: string;
+    style: object;
+    children?: React.ReactNode;
+    hideHandlers?: boolean;
+    hideBorder?: boolean;
+    handlersColor?: string;
+    borderColor?: string;
+    gridBackground?: boolean;
+    onDrag?: Function;
+    initialWidth?: number;
+    initialHeight?: number;
+    initialX?: number;
+    initialY?: number;
 };
-
 
 export type CalcResizableTypes = {
-	className: string;
-	pageX?: number;
-	pageY?: number;
-	prevX?: number;
-	prevY?: number;
-  prevTop?: number;
-	prevWidth?: number;
-	prevHeight?: number;
-	prevLeft?: number;
-	minWidth?: number;
-	minHeight?: number;
-	maxBottom?: number;
-	x?: number;
-	y?: number;
+    className: string;
+    pageX?: number;
+    pageY?: number;
+    prevX?: number;
+    prevY?: number;
+    prevTop?: number;
+    prevWidth?: number;
+    prevHeight?: number;
+    prevLeft?: number;
+    minWidth?: number;
+    minHeight?: number;
+    maxBottom?: number;
+    x?: number;
+    y?: number;
 };
 
-
 export type MaxPositionByParentTypes = {
-	className: string;
-	maxParentBottom: number;
-	maxRight: number;
-	width: number;
-	height: number;
-	x: number;
-	y: number;
-	maxWidth: number;
-	maxHeight: number;
-}
+    className: string;
+    maxParentBottom: number;
+    maxRight: number;
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+    maxWidth: number;
+    maxHeight: number;
+};
